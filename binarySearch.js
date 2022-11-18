78668e240491fa26cf2de44642e3083dc0b53e40
Reinaldo Assis <reinaldoams@gmail.com>
2022-11-17 23:39:56 -0300

    first commit

---
+++ binarySearch.js
@@ -0,0 +1,46 @@
+"use strict";
+const arrayToSearch = Array.from(Array(1000).keys());
+function binarySearch(array, target) {
+    if (target < 0 || target > array[array.length - 1]) {
+        console.log(`Target too large or negative. Could not find ${target}`);
+        return;
+    }
+    let currentIndex = Math.floor(array.length / 2);
+    let top = array.length;
+    let down = 0;
+    while (array[currentIndex] !== target) {
+        // console.log(down,array[currentIndex], top, `currentIndex = ${currentIndex}`)
+        if (array[currentIndex] > target) {
+            top = currentIndex;
+            currentIndex = Math.floor((currentIndex - down) / 2) + down;
+        }
+        else {
+            down = currentIndex;
+            currentIndex += Math.ceil((top - currentIndex) / 2);
+        }
+    }
+    if (array[currentIndex] === target) {
+        console.log(down, currentIndex, top, `number: ${array[currentIndex]} (Found)`);
+    }
+    else {
+        console.log(down, currentIndex, top, `number: ${array[currentIndex]} (Not found)`);
+    }
+}
+// binarySearch(arrayToSearch, 0)
+// binarySearch(arrayToSearch, 1)
+// binarySearch(arrayToSearch, 5)
+// binarySearch(arrayToSearch, 55)
+// binarySearch(arrayToSearch, 100)
+// binarySearch(arrayToSearch, 105)
+// binarySearch(arrayToSearch, 177)
+// binarySearch(arrayToSearch, 500)
+// binarySearch(arrayToSearch, 505)
+// binarySearch(arrayToSearch, 507)
+// binarySearch(arrayToSearch, 508)
+// binarySearch(arrayToSearch, 777)
+// binarySearch(arrayToSearch, 888)
+// binarySearch(arrayToSearch, 999)
+// binarySearch(arrayToSearch, 1000)
+// binarySearch(arrayToSearch, 1001)
+// binarySearch(arrayToSearch, 1002)
+// binarySearch(arrayToSearch, 1003)
